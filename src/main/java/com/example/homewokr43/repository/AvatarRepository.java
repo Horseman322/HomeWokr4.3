ef479b7c84a3e1e981661e533066725feefba09b
Kosarev <kosarevn97@gmail.com>
2022-11-18 22:14:49 +0700

    Initial commit

---
+++ src/main/java/com/example/homewokr43/repository/AvatarRepository.java
@@ -0,0 +1,10 @@
+package com.example.homework42.repository;
+
+import com.example.homework42.entity.Avatar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+
+}
---
+++ src/test/java/com/example/homewokr43/ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.homewokr43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
